43468265e6793d5835b31b66a7343adfe740ea08
Parthiban Nallathambi <parthiban@linumiz.com>
2022-03-10 22:06:56 +0530

    parthiban: review comments
    - all the tags 'pn:' needs to be fixed in one way or another
    - some more changes are manually done from my end also needs to be incorporated
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

--- htu21d/htu21d.h
+++ htu21d/htu21d.h
@@ -9,18 +9,24 @@
 #ifndef ZEPHYR_DRIVERS_SENSOR_HTU21D_H_
 #define ZEPHYR_DRIVERS_SENSOR_HTU21D_H_
 
+/*
+   pn: I don't understand this register. Is this I2C slave address?
+   if so, this should come from devicetree
+*/
+#define HTU21D_I2CADDR		0x40
 
-#define HTU21D_I2CADDR 0x40
+#define HTU21D_READ_TEMP_H	0xE3
+#define HTU21D_READ_HUMID_H	0xE5
+#define HTU21D_READ_TEMP_NH	0xF3
+#define HTU21D_READ_HUMID_NH	0xF5
+#define HTU21D_SOFT_RESET	0xFE
 
-#define HTU21D_SOFT_RESET 0xFE
-#define HTU21D_READ_TEMP_NH 0xF3
-#define HTU21D_READ_HUMID_NH 0xF5
-#define HTU21D_READ_TEMP_H 0xE3
-#define HTU21D_READ_HUMID_H 0xE5
+#define STARTUP_WAIT_MS		15
+#define MEASURE_WAIT_MS		50
 
-#define STARTUP_WAIT_MS 15
-#define MEASURE_WAIT_MS 50
+/* pn: I think this can go to respective function? */
+#define MAX_READ_RETRY_COUNT	5
+/* pn: this is not required, just use CONFIG_HTU21D_NH_MASTER */
+#define NO_HOLD_MASTER_MODE	0
 
-#define MAX_READ_RETRY_COUNT 5
-#define NO_HOLD_MASTER_MODE 0
 #endif
--- htu21d_sample_application/src/main.c
+++ htu21d_sample_application/src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Firmwave
+ * Copyright (c) 2020 Linumiz
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,6 +8,7 @@
 #include <device.h>
 #include <drivers/sensor.h>
 #include <sys/printk.h>
+/* pn: we shouldn't need this AFAIK */
 #include <sys/__assert.h>
 
 static void do_main(const struct device *dev)
@@ -15,25 +16,30 @@ static void do_main(const struct device *dev)
 	int ret;
 	struct sensor_value temp_value;
 	struct sensor_value hum_value;
+	/* pn: should have new line here and this applies to whole file */
 	while (1) {
 		ret = sensor_sample_fetch(dev);
 		if (ret) {
 			printk("sensor_sample_fetch failed ret %d\n", ret);
 			return;
 		}
+		/* pn: should have new line here and this applies to whole file */
 		ret = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp_value);
 		if (ret) {
 			printk("sensor_channel_get failed ret %d\n", ret);
 			return;
 		}
+		/* pn: should have new line here and this applies to whole file */
 		printk("temp is %d (%d micro)\n", temp_value.val1,
 		       temp_value.val2);
 		k_sleep(K_MSEC(1000));
+		/* pn: should have new line here and this applies to whole file */
 		ret = sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &hum_value);
                 if (ret) {
                         printk("sensor_channel_get failed ret %d\n", ret);
                         return;
                 }
+		/* pn: should have new line here and this applies to whole file */
                 printk("humidity is %d (%d micro)\n", hum_value.val1,
                        hum_value.val2);
                 k_sleep(K_MSEC(1000));
@@ -42,10 +48,14 @@ static void do_main(const struct device *dev)
 
 void main(void)
 {
+	/* pn: I think we should get label macro here instead of hardcoded value */
 	const struct device *dev = device_get_binding("HTU21D");
+	/* pn: spacing issue */
+	/* pn: should have new line here and this applies to whole file */
 	if(dev==NULL){
 		printk("Failed to get the device\n");
 		return;
 	}
+	/* pn: should have new line here and this applies to whole file */
 	do_main(dev);
 }
